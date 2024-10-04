9552894e270013dbab4aad6fafacfaf304e24ae2
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-04 20:06:55 +0200

    homes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import { CategoryItemsFilter } from "@/components/category/CategoryItemsFilter";
 import React from "react";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: {
+    category: string | undefined | null;
+  }
+}
+
+export default function Home({ searchParams }: HomeProps) {
+  const category = searchParams.category as string | undefined;
+  
   return (
     <div className="px-10">
       <CategoryItemsFilter />
