cc01ee489ddea10722ed95ceb75bfaaf2fcad0f0
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-05 13:22:47 +0200

    structure

--- src/app/host/structure/page.tsx
+++ src/app/host/structure/page.tsx
@@ -1,3 +1,4 @@
+import { NextStep } from '@/components/newAirBnBHome/structureForm/NextStep'
 import { SelectCategory } from '@/components/newAirBnBHome/structureForm/SelectCategory'
 import React from 'react'
 
@@ -6,6 +7,8 @@ const PageStuctureCategory = () => {
     <section className="max-w-[1300px] mx-auto mt-7">
         <h1 className="text-3xl font-bold">Which of these best describe your home?</h1>
         <SelectCategory />
+        
+        <NextStep />
     </section>
   )
 }
--- src/components/newAirBnBHome/structureForm/SelectCategory.tsx
+++ src/components/newAirBnBHome/structureForm/SelectCategory.tsx
@@ -18,7 +18,7 @@ export const SelectCategory = () => {
         onClick={() => handleCategory(item.name)}
         key={item.id} className={cn(
             "w-[220px] cursor-pointer p-5 py-7 border-2 border-gray-200 rounded-lg",
-            category === item.name ? "border-2 border-red-300" : ""
+            category === item.name ? "border-red-300 border-2" : ""
         )}>
             <img src={item.imageUrl} alt={item.name} className="w-10 h-10" />
             <p className="text-sm font-bold mt-1">{item.title}</p>
