0f737e108607f797fffb4fded375e31f3e2d239d
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-10 13:31:39 +0200

    price

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -1,10 +1,48 @@
 import { getUserBookings } from "@/server/Reservation"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+import { format, differenceInDays } from "date-fns"
 
 const MyReservationsPage = async () => {
   const bookings = await getUserBookings({})
   
+  const nightPrice = (booking: any) => {
+    const startDate = booking.startDate
+    const endDate = booking.endDate
+    const totalNights = differenceInDays(endDate, startDate)
+    return totalNights * booking.home.price
+  }
+  
   return (
-    <div>MyReservationsPage</div>
+    <section className="py-10 max-w-[1400px] mx-auto">
+        <h1 className="text-2xl font-bold mb-10">My reservations</h1>
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Home title</TableHead>
+                    <TableHead>Check in date</TableHead>
+                    <TableHead>Check out date</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Price</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {bookings?.data?.map((booking) => (
+                    <TableRow key={booking.home.id}>
+                        <TableCell>{booking.home.title}</TableCell>
+                        <TableCell>{format(booking.startDate, "MMM dd, yyyy")}</TableCell>
+                        <TableCell>{format(booking.endDate, "MMM dd, yyyy")}</TableCell>
+                        <TableCell>{booking.status}</TableCell>
+                        <TableCell>{nightPrice(booking)}</TableCell>
+                        <TableCell className="flex gap-2">
+                            <Button variant="destructive">Cancel</Button>
+                            <Button>Review</Button>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </section>
   )
 }
 
